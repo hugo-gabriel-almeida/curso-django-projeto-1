2967e1db6a06eb1ffeeaf561a1f304233ca692a4
Hugo Almeida <hugo_gabriel_almeida@hotmail.com>
2023-08-26 11:41:11 -0300

    removed contact us and about

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from recipes.views import about, contact_us, home
+from recipes.views import home
 
 urlpatterns = [
     path('', home),
-    path('contact-us', contact_us),
-    path('about', about)
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -6,11 +6,3 @@ def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Hugo Almeida',
     })
-
-
-def contact_us(request):
-    return HttpResponse('contact us')
-
-
-def about(request):
-    return HttpResponse('about')
